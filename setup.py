89ece8a014815dc3936fcb2fdad18e2b8154051f
julienbarrier <1346858+julienbarrier@users.noreply.github.com>
2021-08-18 12:42:58 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='station2004',
+      version='0.0.1',
+      description='Station files for the CMP group at Manchester',
+      url='https://github.com/julienbarrier/qinst',
+      author='Julien Barrier',
+      author_email='julien.barrier@manchester.ac.uk',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3 :: Only',
+          'License :: Apache License',
+          'Topic :: Scientific/Engineering',
+      ],
+      license='MIT',
+      packages=find_packages(),
+      python_requires='>=3.9',
+      zip_safe=False)
